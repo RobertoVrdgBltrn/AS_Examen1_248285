11b2feb6d3592307e765e1bbea9f7a1827257285
Roberto Verdugo Beltran <159956806+RobertoVrdgBltrn@users.noreply.github.com>
2026-02-27 12:42:39 -0700

    Se creo el proyecto

---
+++ AS_Examen1_248285/src/main/java/itson/as_examen1_248285/AS_Examen1_248285.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.as_examen1_248285;
+
+/**
+ *
+ * @author rober
+ */
+public class AS_Examen1_248285 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
